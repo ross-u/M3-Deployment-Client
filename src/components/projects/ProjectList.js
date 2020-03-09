8f58772ff49b670414a11e71770603ef5baf8992
ross-u <cirkovic.uros.srb@gmail.com>
2020-03-09 14:52:47 +0100

    Add comment to ProjectList axios call

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,23 +1,24 @@
 // components/projects/ProjectList.js
 
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import projectAPI from './../../lib/project-services'
+import projectAPI from "./../../lib/project-services";
 
-import AddProject from './AddProject'; // <== !!!
+import AddProject from "./AddProject"; // <== !!!
 
 class ProjectList extends Component {
-	state = { 
-    listOfProjects: [] 
+  state = {
+    listOfProjects: []
   };
 
   getAllProjects = () => {
-    projectAPI.getAllProjects()
-     .then((data) => {
-        this.setState({ listOfProjects: data })
-      })  
-  }
+    // // OR if not using a service:
+    //  axios.get(process.env.REACT_APP_API_URL + '/api/projects')
+    projectAPI.getAllProjects().then(data => {
+      this.setState({ listOfProjects: data });
+    });
+  };
 
   componentDidMount() {
     //  fetch the data from API befor initial render
@@ -27,25 +28,25 @@ class ProjectList extends Component {
   render() {
     const { listOfProjects } = this.state;
 
-    return(
-      <div>         {/* After adding a projects,we will GET all projects again from API  */}
-        <AddProject getData={this.getAllProjects} />   
+    return (
+      <div>
+        {" "}
+        {/* After adding a projects,we will GET all projects again from API  */}
+        <AddProject getData={this.getAllProjects} />
         <div>
-          { 
-            listOfProjects.map( (project) => {
+          {listOfProjects.map(project => {
             return (
-              <div key={project._id} className='project'>
+              <div key={project._id} className="project">
                 <Link to={`/projects/${project._id}`}>
                   <h3>{project.title}</h3>
                   <p>{project.description} </p>
                 </Link>
               </div>
-            )})
-          }
+            );
+          })}
         </div>
-
       </div>
-    )
+    );
   }
 }
 
